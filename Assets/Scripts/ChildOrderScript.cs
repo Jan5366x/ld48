bcf45cd15db5683ed5525fea61bd282493c14986
Jan Schwien <jan5366@gmail.com>
2021-04-24 16:31:04 +0200

    Add proper sorting; Add tree; Add ground placeholder

---
+++ Assets/Scripts/ChildOrderScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ChildOrderScript : MonoBehaviour
+{
+    private const double ErrorTimeout = 5d;
+    private const double ScriptCheckTimeout = 2d;
+    public int childLayerOffset = 0;
+    private OrderScript _parentScript;
+    private SpriteRenderer _spriteRenderer;
+    private bool _hasOwnRenderer = false;
+    private bool _parentScriptFound = false;
+
+    private double _errorTimer = ErrorTimeout;
+    private double _scriptCheckTimer = 0d;
+    private Transform _lastParent = null;
+    
+    void Awake() => PrepareSpriteRenderer();
+
+    void Update()
+    {
+        // update sprite renderer and parent script if in Editor mode to provide live updates
+        if(!Application.isPlaying)
+            PrepareSpriteRenderer();
+        
+        if (_scriptCheckTimer > 0)
+        {
+            _errorTimer -= Time.deltaTime;
+        }
+        else
+        {
+            // check if script must be searched again
+            if (_lastParent != transform.parent)
+            {
+                _parentScriptFound = false;
+                FindParentScript();
+                _lastParent = transform.parent;
+            }
+
+            _scriptCheckTimer = ScriptCheckTimeout;
+        }
+        
+        
+        // final update hook
+        if (_hasOwnRenderer && _parentScriptFound)
+        {
+            _spriteRenderer.sortingOrder = _parentScript.GetSortingOrder() + childLayerOffset;
+        }
+        else 
+        {
+            if (_errorTimer > 0)
+            {
+                _errorTimer -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.LogError("Missing parent sorting order script or sprite renderer!");
+                _errorTimer = ErrorTimeout;
+            }
+        }
+    }
+
+    private void PrepareSpriteRenderer()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _hasOwnRenderer = _spriteRenderer != null;
+    }
+    
+    private void FindParentScript()
+    {
+        var current = transform.parent;
+        while (!_parentScriptFound && current != null)
+        {
+            var script = current.GetComponent<OrderScript>();
+
+            if (script != null)
+            {
+                _parentScript = script;
+                _parentScriptFound = true;
+                break;
+            }
+
+            current = current.parent;
+        }
+    }
+}
--- Assets/Scripts/OrderScript.cs
+++ Assets/Scripts/OrderScript.cs
@@ -2,18 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class OrderScript : MonoBehaviour
 {
-    public float offset;
+    private const int ChildLayerSpace = 100;
+
+    public float groundOffset;
+
+    private int _sortingOrder = 0;
     private SpriteRenderer _spriteRenderer;
+    private bool _hasOwnRenderer = false;
     
-    void Awake()
+    private void Awake()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
+        Prepare();
         SetPosition();
     }
 
-    void Update() => SetPosition();
+    void Update()
+    {
+        SetPosition();
+        
+        var objPos = transform.position;
+        
+        Debug.DrawLine(
+            new Vector3(objPos.x + -0.3f,objPos.y + groundOffset), 
+            new Vector3(objPos.x + 0.3f,objPos.y + groundOffset),
+            _hasOwnRenderer ? Color.yellow : Color.magenta);
+    }
 
-    void SetPosition() => _spriteRenderer.sortingOrder = (int)-((transform.position.y + offset) * 100);
+    void SetPosition()
+    {
+        
+        // update sprite renderer if in Editor mode to provide live updates
+        if(!Application.isPlaying)
+            Prepare();
+        
+        _sortingOrder = (int) -((transform.position.y + groundOffset) * ChildLayerSpace);
+        
+        if (_hasOwnRenderer)
+            _spriteRenderer.sortingOrder = _sortingOrder;
+    }
+
+    private void Prepare()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _hasOwnRenderer = _spriteRenderer != null;
+    }
+
+    public int GetSortingOrder()
+    {
+        return _sortingOrder;
+    }
 }

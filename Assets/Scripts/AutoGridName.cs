24f37dbf39f24091770e6802bf5d09257cd155cb
Jan Schwien <jan5366@gmail.com>
2021-04-24 10:36:03 +0200

    Add worldspace prefab; Add auto grid name script

---
+++ Assets/Scripts/AutoGridName.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AutoGridName : MonoBehaviour
+{
+    private Vector3 _lastLocation;
+    
+    void Awake () {
+        AutoName();
+    }
+    
+    void Update () {
+        AutoName();
+    }
+
+    private void AutoName()
+    {
+        if (transform.position.Equals(_lastLocation)) return;
+        
+        var position = transform.position;
+        name = "G-" + (int)position.x + "-" + (int)-position.y;
+
+        _lastLocation = new Vector3(position.x, position.y, position.z);
+    }
+}

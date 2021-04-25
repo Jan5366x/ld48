bdd2078b4dca600024dc18ef701b62962dca59ac
Codexzier <johannes.paul.langner@live.de>
2021-04-25 10:53:27 +0200

    Change resize hud by resulution
    Some problem with the, if the solution to small or to great.

--- Assets/Scripts/UI/HUD/HeadUpDisplay.cs
+++ Assets/Scripts/UI/HUD/HeadUpDisplay.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UI.MainMenu;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeadUpDisplay : MonoBehaviour
 {
+    public Camera Camera;
+
+    public GameObject Top;
+    public GameObject Bottom;
+    
+    
+    public Slider Health;
+    public Slider Stamina;
+    public Slider InfectionState;
+
+    public TMP_Text CoinCounter;
+    public TMP_Text InfectedTime;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        this.SetScaleUiElements();
+        this.SetStateValues(1f, 1f, 1f);
+        this.CoinCounter.text = "0";
+        this.InfectedTime.text = "00:00:00";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Test
+        this.InfectedTime.text = $"{DateTime.Now:hh:mm:ss}";
+        this.SetScaleUiElements();
+    }
+
+    private void SetScaleUiElements()
+    {
+        this.Top.ScaleByAspectAndPixelHeight(this.Camera, 1000);
+        this.Bottom.ScaleByAspectAndPixelHeight(this.Camera, 1000);
+    }
+
+    private void SetStateValues(float health, float stamina, float infectionState)
+    {
+        SetValueIfBetweenMinMax(this.Health, health);
+        SetValueIfBetweenMinMax(this.Stamina, stamina);
+        SetValueIfBetweenMinMax(this.InfectionState, infectionState);
+
+        static void SetValueIfBetweenMinMax(Slider slider, float value)
+        {
+            if (value >= slider.minValue && value <= slider.maxValue)
+            {
+                slider.value = value;   
+            }
+        }
     }
 }
--- Assets/Scripts/UI/MainMenu/ResolutionHelper.cs
+++ Assets/Scripts/UI/MainMenu/ResolutionHelper.cs
@@ -5,10 +5,31 @@ namespace UI.MainMenu
     public static class ResolutionHelper
     {
         public const float DefaultRatio = 1.7f;
+        public const float DefaultPixelHeight = 1080;
 
+        public static void ScaleByAspect(this GameObject gameObject, Camera camera)
+        {
+            gameObject.ScaleByAspect(camera.aspect);
+        }
+        
         public static void ScaleByAspect(this GameObject gameObject, float cameraAspect)
         {
             gameObject.transform.localScale = new Vector3(1, 1, 1) * cameraAspect / DefaultRatio;
+        }
+
+        public static void ScaleByAspectAndPixelHeight(this GameObject gameObject, Camera camera, int minWidth)
+        {
+            var byPixelHeight = camera.pixelHeight / DefaultPixelHeight;
+            Debug.Log($"Pixel Height: {byPixelHeight}");
+
+            float scaleWidth = 1;
+            
+            if (camera.pixelWidth < minWidth)
+            {
+                scaleWidth = camera.pixelWidth / (float)minWidth;
+            }
+            
+            gameObject.transform.localScale = new Vector3(1, 1, 1) * (byPixelHeight * scaleWidth);
         }
 
         public static void ScalePositionByAspect(this GameObject gameObject, float camerAspect)

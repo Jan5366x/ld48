80cc3904be7df46e9630f677319caf7527f766cf
niels <kormarun@gmail.com>
2021-04-25 13:58:03 +0200

    Spawn Enemies

---
+++ Assets/Scripts/Entity/EnemySpawner.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public SpawnerWave[] waves;
+    public bool playerInRange;
+    public float spawnTimer = 0;
+
+    private void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer < 0 && playerInRange)
+        {
+            var spawnerWave = waves[Random.Range(0, waves.Length)];
+            SpawnAround(spawnerWave);
+            spawnTimer = Random.Range(spawnerWave.delayMin, spawnerWave.delayMax);
+        }
+    }
+
+    void SpawnAround(SpawnerWave wave)
+    {
+        int count = Random.Range(wave.countMin, wave.countMax);
+        for (int i = 0; i < count; i++)
+        {
+            var position = transform.position;
+            var range = wave.spawnerRange;
+            float spawnX = Random.Range(position.x - range.x, position.x + range.x);
+            float spawnY = Random.Range(position.y - range.y, position.y + range.y);
+
+            Instantiate(wave.prefab, new Vector3(spawnX, spawnY), Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Player>())
+        {
+            playerInRange = true;
+            transform.parent.GetComponent<Animator>().SetBool("Active", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Player>())
+        {
+            playerInRange = false;
+            transform.parent.GetComponent<Animator>().SetBool("Active", false);
+        }
+    }
+}
